f7b297b6b0ae9119107361e4f9dcc3641c116760
iri.sobakina <iri.dolgopolova@gmail.com>
2022-11-08 10:29:55 +0300

    Добавляет чатик с автоматическим ответом бота

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,18 +1,84 @@
 import './App.css';
 import Message from './components/Message'
-
-const myText = 'Hello, Irina!';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const myText = 'Привет, напиши нашему боту!';
+  const currentUsername = 'Пользователь';
+  const botUsername = 'Бот';
+
+  let newMessage;
+  const [messageList, setMessageList] = useState([]);
+
+  const isCurrentUser = (username) => {
+    return username === currentUsername;
+  }
+
+  const renderMessageList = () => {
+    return messageList.map((message, index) => (
+      <p key={index} className={`App-message-list__row ${isCurrentUser(message.author) ? 'right' : ''}`} >
+        <span className={`App-message-list ${isCurrentUser(message.author) ? 'user' : ''}`}>
+          {message.text}
+        </span>
+      </p>
+    ));
+  }
+
+  const updateMessageList = (event) => {
+    event.preventDefault();
+
+    if (newMessage) {
+      setMessageList([
+        ...messageList,
+        {
+          text: newMessage,
+          author: currentUsername
+        }
+      ]);
+
+      document.myForm.reset();
+    }
+  }
+
+  const handleChange = (event) => {
+    newMessage = event.target.value;
+  }
+
+  useEffect(() => {
+    if (messageList.length && isCurrentUser(messageList.slice(-1)[0].author)) {
+      setTimeout(() => {
+        setMessageList([
+          ...messageList,
+          {
+            text: "На данный момент бот не доступен, попробуйте позже",
+            author: botUsername
+          }
+        ]);
+      }, 1500);
+    }
+  }, [messageList]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="App-header__card">
-          <span>My First React App</span>
+      <div className="App-main">
+        <div className="App-main__card">
           <Message text={myText} />
-        </div>
 
-      </header>
+          <div className="App-message-list__history">{renderMessageList()}</div>
+
+          <form className='App-form' name="myForm" onSubmit={updateMessageList}>
+            <input
+              className="App-form__input"
+              type="text"
+              value={newMessage}
+              onChange={handleChange}
+              placeholder="Сообщение..."
+            />
+
+            <button className="App-form__btn" type="submit">Отправить</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
